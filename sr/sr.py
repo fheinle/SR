62cc6ebd412aa51a1c5ed521d5a37adcbc759ac2
Florian Heinle <florian-launchpad@planet-tiax.de>
2008-07-17 15:18:18 +0200

    [project @ florian-launchpad@planet-tiax.de-20080717131818-roeviv4tbh6mexnd] changed configfile creation

--- sr/sr.py
+++ sr/sr.py
@@ -16,25 +16,32 @@ from libsr import Project
 def create(directory):
     """
     create a new directory and the subdirectories needed for usage with sr
+    
+    @param directory: path where to create a new project
+    @type directory: string
     """
     os.makedirs(directory)
     os.makedirs(os.path.join(directory, 'source'))
     os.makedirs(os.path.join(directory, 'templates'))
     os.makedirs(os.path.join(directory, 'output'))
-    configfile = open(os.path.join(directory, 'config.ini'), 'w')
-    config = SafeConfigParser()
-    config.add_section('markdown')
-    config.add_section('general')
-    config.add_section('navigation')
-    config.set('markdown', 'safe', 'False')
-    config.set('markdown', 'addons', 'codehilite,')
-    config.set('general', 'suffix', '.txt')
-    config.set('navigation', 'index', 'index.html')
-    config.write(configfile)
+    
+    with open(os.path.join(directory, 'config.ini'), 'w') as configfile:
+        config = SafeConfigParser()
+        config.add_section('markdown')
+        config.add_section('general')
+        config.add_section('navigation')
+        config.set('markdown', 'safe', 'False')
+        config.set('markdown', 'addons', 'codehilite,')
+        config.set('general', 'suffix', '.txt')
+        config.set('navigation', 'index', 'index.html')
+        config.write(configfile)
 
 def list_changed(project):
     """
     print all files that need re-rendering in given project
+
+    @param project: project to check for changed files
+    @type project: project object from libsr
     """
     print "Files that need re-rendering: "
     for changed_page in project.list_changed()[0]:
