637237a8c93a8c2e84a5bbee62586bdd3333f988
Florian Heinle <florian-launchpad@planet-tiax.de>
2008-08-06 01:01:29 +0200

    [project @ florian-launchpad@planet-tiax.de-20080805230129-dm4cp9qo9ep2vwgh] added setup.py, fixes #248687

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+""" setup file for static render """
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name = 'StaticRender',
+    version = '0.1',
+    author = 'Florian Heinle',
+    description = '''SR means to provide easy methods for rendering plaintext
+    files with human readable formatting into static html files, for situations
+    when no complete/dynamic CMS is necessary.''',
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': ['sr = sr.sr:main',]
+    },
+    platforms = 'any',
+    include_package_data = True,
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: End Users/Desktop',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    install_requires = 'markdown',
+)
